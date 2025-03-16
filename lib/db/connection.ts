fc1de764a4ac0989115911c0cbea43a3a0a5a50e
MinUk <qjvk2880@gmail.com>
2025-03-16 21:25:26 +0900

    MongoDB 연결 타입 에러 수정: cached 타입 정의 및 Promise 반환값 처리 개선

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -2,18 +2,19 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pixel-art';
 
-// TypeScript를 위한 global 타입 정의
+type CachedType = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
 declare global {
-  var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-  } | undefined;
+  var mongoose: CachedType | undefined;
 }
 
-let cached = global.mongoose;
+let cached: CachedType = global.mongoose || { conn: null, promise: null };
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+if (!global.mongoose) {
+  global.mongoose = cached;
 }
 
 export async function connectToDatabase() {
@@ -27,9 +28,10 @@ export async function connectToDatabase() {
     };
 
     console.log('MongoDB 연결 중...');
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
       console.log('MongoDB 연결 성공!');
-      return mongoose;
+      cached.conn = mongooseInstance;
+      return mongooseInstance;
     });
   }
   
